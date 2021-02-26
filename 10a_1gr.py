e42c48a0befebac35451c0b5e10cf6931cf111b9
JanaMinalto <79309478+JanaMinalto@users.noreply.github.com>
2021-02-26 12:15:10 +0200

    Update 10a_1gr.py

--- 10a_1gr.py
+++ 10a_1gr.py
@@ -1,24 +1,24 @@
-1 Arhipenko Sofja
+#1 Arhipenko Sofja
 
-2 Berga Laura
+#2 Berga Laura
 
-3 Fiïimonovs Eldars
+#3 Fiïimonovs Eldars
 
-4 Gaiðute Ksenija
+#4 Gaiðute Ksenija
 
-5 Gorniks Arturs
+#5 Gorniks Arturs
 
-6 Guþonoka Jevgeòija
+#6 Guþonoka Jevgeòija
 
-7 Isaèenko Aleksandrs
+#7 Isaèenko Aleksandrs
 
-8 Komaðilo Dmitrijs
+#8 Komaðilo Dmitrijs
 
-9 Lukjanèikova Darja
+#9 Lukjanèikova Darja
 
-10 Minalto Jana
-print(5*2)
+#10 Minalto Jana
+print(5*2/2)
 
-11 Obrazcova Marija
+#11 Obrazcova Marija
 
-12 Ðvacka Marija
+#12 Ðvacka Marija
